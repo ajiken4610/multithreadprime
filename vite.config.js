fd136dbe86a33393e0c2ebf685c349f7ba445ee1
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-11-23 16:19:55 +0900

    viteコンフィグを追加

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  build: {
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "es2020"
+    }
+  }
+})
